eff4c54eb0c4f266c88cd01ff24a88c6ba237ed9
haruyasu <harukun2002@gmail.com>
2020-12-21 09:57:00 +0900

    Cards component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import Cards from './features/covid/Cards/Cards';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <Cards />
       </header>
     </div>
   );
